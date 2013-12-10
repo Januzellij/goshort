914049ebe47489cfd3e13e9306a78b0bd54ac58e
Jake Januzelli <jake_boaconstrictor@yahoo.com>
2013-12-10 17:18:54 -0500

    stupid indents

--- server.go
+++ server.go
@@ -1,14 +1,14 @@
 package goshort
 
 import (
-    "fmt"
-    "net/http"
-    "github.com/gorilla/mux"
-    "errorH"
-    "html/template"
-    "crypto/sha1"
-    "appengine"
-    "appengine/datastore"
+	"fmt"
+	"net/http"
+	"github.com/gorilla/mux"
+	"errorH"
+	"html/template"
+	"crypto/sha1"
+	"appengine"
+	"appengine/datastore"
 )
 
 // this is the type of entity stored in the datastore
@@ -25,31 +25,31 @@ var templates = template.Must(template.ParseFiles(
 
 // sets up http handlers with urls
 func init() {
-    r := mux.NewRouter()
-    r.HandleFunc("/", errorH.Handle(rootH))
-    r.HandleFunc("/{urlId}", errorH.Handle(urlH))
-    http.Handle("/", r)
+	r := mux.NewRouter()
+	r.HandleFunc("/", errorH.Handle(rootH))
+	r.HandleFunc("/{urlId}", errorH.Handle(urlH))
+	http.Handle("/", r)
 }
 
 // this function shows a form to enter a url to be shortened
 // it generates a url id, which should be copied and pasted after the goshort address
 // example when running goshort on port 8080: localhost:8080/urlId
 func rootH(w http.ResponseWriter, r *http.Request) {
-    if r.Method != "POST" {
+	if r.Method != "POST" {
 		templates.ExecuteTemplate(w, "index.html", nil)
 	} else {
 		shortUrl := r.FormValue("shortUrl")
 		id := genUrlId([]byte(shortUrl))
 		c := appengine.NewContext(r)
 		key := datastore.NewKey(c, "Url", id, 0, nil)
-        url := new(Url)
-        err := datastore.Get(c, key, url)
+		url := new(Url)
+		err := datastore.Get(c, key, url)
 		if err == datastore.ErrNoSuchEntity {
-        	newUrl := &Url{shortUrl, id}
-       		_, err := datastore.Put(c, key, newUrl)
-        	errorH.Check(err)
-    	}
-        templates.ExecuteTemplate(w, "index.html", id)
+			newUrl := &Url{shortUrl, id}
+			_, err := datastore.Put(c, key, newUrl)
+			errorH.Check(err)
+		}
+		templates.ExecuteTemplate(w, "index.html", id)
 	}
 }
 
@@ -62,13 +62,13 @@ func urlH(w http.ResponseWriter, r *http.Request) {
 	
 	c := appengine.NewContext(r)
 	key := datastore.NewKey(c, "Url", urlId, 0, nil)
-    queryUrl := new(Url)
-    err := datastore.Get(c, key, queryUrl)
-    if err == datastore.ErrNoSuchEntity {
-    	templates.ExecuteTemplate(w, "error.html", "this id does not correspond with a url")
-    } else {
-        http.Redirect(w, r, queryUrl.OrigUrl, http.StatusFound)
-    }
+	queryUrl := new(Url)
+	err := datastore.Get(c, key, queryUrl)
+	if err == datastore.ErrNoSuchEntity {
+		templates.ExecuteTemplate(w, "error.html", "this id does not correspond with a url")
+	} else {
+		http.Redirect(w, r, queryUrl.OrigUrl, http.StatusFound)
+	}
 }
 
 // this function hashes a url and prints the first 8 characters, to generate a unique id
